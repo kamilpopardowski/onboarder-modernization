ef6b35220ae39dd861bf756ed09a1a7384b517ab
Kamil Popardowski <kamil.popardowski@protonmail.com>
2025-11-19 08:54:20 -0600

    Add new entities for lookups

---
+++ Legacy/LegacyOnboarder/Models/Department.cs
@@ -0,0 +1,8 @@
+namespace LegacyOnboarder.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+
+    public string DepartmentName { get; set; }
+}
---
+++ Legacy/LegacyOnboarder/Models/EmployeeType.cs
@@ -0,0 +1,8 @@
+namespace LegacyOnboarder.Models;
+
+public enum EmployeeType
+{
+    FullTime = 1,
+    Contractor = 2,
+    Intern = 3
+}
