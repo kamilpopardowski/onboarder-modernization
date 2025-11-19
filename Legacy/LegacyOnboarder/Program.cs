7b253d01b70a079784ee6d6491cfc43b7727c1ee
Kamil Popardowski <kamil.popardowski@protonmail.com>
2025-11-19 04:06:46 -0600

    Introduce onboarding/offboarding basic functionality

--- Legacy/LegacyOnboarder/Program.cs
+++ Legacy/LegacyOnboarder/Program.cs
@@ -1,5 +1,8 @@
 using LegacyOnboarder.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
